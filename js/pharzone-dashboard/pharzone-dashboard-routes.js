36582881179f60c18a2caf436065d121b0d05370
irthos <irth03@gmail.com>
2015-08-02 13:23:06 -0700

    dashboard beginning

--- js/pharzone-dashboard/pharzone-dashboard-routes.js
+++ js/pharzone-dashboard/pharzone-dashboard-routes.js
@@ -10,13 +10,10 @@
       url: '/dashboard',
       template: '<ui-view></ui-view>'
     }).state('pharzone.dashboard.edit', {
-      url: '/edit',
-      template: '<edit-products></edit-products><ui-view></ui-view>'
-    }).state('pharzone.dashboard.edit.product', {
-      url: '/:product',
+      url: '/products',
       template: '<edit-products></edit-products>'
     }).state('pharzone.dashboard.account', {
-      url: '/:user',
+      url: '/user/:user',
       template: '<account></account>'
     });
   }
